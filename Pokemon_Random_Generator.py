9f15066a4aada4801f65a7500c1e0fe48ba28c87
Andrew Dass <dassandrew3@gmail.com>
2022-12-03 20:33:03 -0500

    Update Pokemon_Random_Generator.py

--- Pokemon_Random_Generator.py
+++ Pokemon_Random_Generator.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 import random
 import os
-import homepage
+from PIL import Image
 
 
 #Add a lot of text within the function, import py file use send from directory
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    os.system('homepage.py')
+    return render_template('homepage.html')
     
 
 def chosen_pokemon():
@@ -24,26 +24,28 @@ def chosen_pokemon():
     if pick_the_region == 'Kanto':
         pokemon_entry = random.randint(1, 151)
     elif pick_the_region == 'Johto':
-        pokemon_entry == random.randint(152, 251)
+        pokemon_entry = random.randint(152, 251)
     elif pick_the_region == 'Hoenn':
-        pokemon_entry == random.randint(252, 386)
+        pokemon_entry = random.randint(252, 386)
     elif pick_the_region == 'Sinnoh':
-        pokemon_entry == random.randint(387, 493)
+        pokemon_entry = random.randint(387, 493)
     else:
         print("An error has occurred. Run the program again")
-    
+        
     str_pokemon_entry = str(pokemon_entry)
-    
+        
     if pokemon_entry <= 9:
-        str_pokemon_entry = "00" + pokemon_entry
+        str_pokemon_entry = "00" + str_pokemon_entry
     elif pokemon_entry >= 10 and pokemon_entry <= 99:
-        str_pokemon_entry = "0" + pokemon_entry
+        str_pokemon_entry = "0" + str_pokemon_entry
+
+    path = r".\static\Pokemon_Images\{}\{}.png".format(pick_the_region, str_pokemon_entry)
+    img = Image.open(path)
+    img.show()
 
 
-    retrieve_pokemon_image = os.path("./"+region_names+"/"+pokemon_entry+".png")
-
-
-
+if __name__== '__main__':
+    app.run()
     
 if __name__== '__main__':
     app.run()
