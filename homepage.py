386b3ba15cac9b0e09be75863e75ed7eef288bf8
Andrew Dass <dassandrew3@gmail.com>
2022-12-05 21:58:28 -0500

    Delete homepage.py

--- homepage.py
+++
@@ -1,9 +0,0 @@
-import random
-import tkinter import *
-
-print("Hello! Welcome to the Pokemon Random Generator!")
-
-# Add a button
-# Connect it to local directory with the images
-# Use random to get a number, convert to string then paste it in long sentence .. to return the image
-
